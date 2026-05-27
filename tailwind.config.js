2836bffc1306306fb9ca60483898a7e3d71c7dc4
Manu <manusasso10@gmail.com>
2026-05-27 16:18:05 +0200

    resolve merge conflict tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default {
   theme: {
     extend: {
@@ -7,15 +6,6 @@ export default {
         "dark-text": "var(--color-dark-text)",
         "dark-accent": "var(--color-dark-accent)",
       },
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  theme: {
-    colors: {
-      "dark-bg": "rgb(var(--color-dark-bg) / <alpha-value>)",
-      "dark-text": "rgb(var(--color-dark-text) / <alpha-value>)",
-      "dark-accent": "rgb(var(--color-dark-accent) / <alpha-value>)",
->>>>>>> ebad77597a92f636e6dc9fdf180fad7700042e8f
     },
   },
 };
